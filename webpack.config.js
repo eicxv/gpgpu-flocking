90f1dd945cb2302d532bbdc9f437e26f7ca419f4
Einar Persson <einar.jpg@gmail.com>
2020-10-16 19:09:16 +0200

    added npm dependencies and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// webpack.config.js
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
